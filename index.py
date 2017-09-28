c31551d7638577d649f08456a45f7208482b23a7
jochem33 <jochemjuriaan.baas@coderclass.nl>
2017-09-28 09:55:15 +0200

    added pseudo code

---
+++ index.py
@@ -0,0 +1,46 @@
+'''
+pseudocode
+
+Voorbereiding, variabeles maken enzo
+
+
+
+while niet geraden Loop:
+
+    Print voorgang: galg, streepjes en al geraden letters
+
+    Vraag om een letter
+
+    # onderstaande kan ook in een switch case
+
+    if letter is ?
+        print: raad een woord
+        if geraden woord = geheim woord
+            Je hebt het goed geraden!
+            Break
+        else
+            print je hebt het niet goed, ga verder met raden
+
+    if twee letters
+        print je hebt twee letters geraden, dat mag niet, probeer het opnieuw!
+
+    elif letter in geraden letters:
+        print: je hebt deze letter al geraden
+
+    elif letter is in het woord:
+        print goed geraden!
+        werk voortgang bij: zet de letter op de streepjes
+        if woord is af
+            break
+
+    if letter is niet in woord:
+        print dat is niet goed
+        werk voortgang bij: zet letter bij geraden en bouw de galg verder op
+
+
+if geraden
+    print Gefeliciteerd, je hebt het woord geraden!
+else
+    print je heb het helaas niet geraden, het woord was: geheim woord
+
+'''
